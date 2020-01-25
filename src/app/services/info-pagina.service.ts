cfd8e9132189875a0726b837ef4a6c063568f359
Hugo Martinez <hmmartinez88@gmail.com>
2020-01-25 18:14:30 -0500

    fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,30 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
-
     });
-
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-e325f.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
 }
