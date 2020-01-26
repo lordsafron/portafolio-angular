b00a172409e2b0555ae4e01d2f09ab9992f0b826
Hugo Martinez <hmmartinez88@gmail.com>
2020-01-25 20:47:49 -0500

    fin sección 8

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -11,6 +11,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
     this.cargarProductos();
@@ -18,14 +19,58 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://angular-html-e325f.firebaseio.com/productos_idx.json')
-        .subscribe( (resp: Producto[]) => {
+    return new Promise( ( resolve, reject ) => {
 
-          console.log(resp);
+      this.http.get('https://angular-html-e325f.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
           this.productos = resp;
           this.cargando = false;
-
+          resolve();
         });
+
+    });
+
+  }
+
+  getProducto(id: string) {
+
+    return this.http.get(`https://angular-html-e325f.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then ( ()=> {
+        // ejecutar después de tener los productos
+        // Aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
 
   }
 
